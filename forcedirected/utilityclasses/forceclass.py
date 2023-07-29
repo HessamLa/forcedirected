31bfce68c6d8339386918172a9bf7e729255d445
H_ALZ <hessam.la@gmail.com>
2023-07-29 19:36:46 -0400

    ForceClass is to include dimension coefficient vector.

--- forcedirected/utilityclasses/forceclass.py
+++ forcedirected/utilityclasses/forceclass.py
@@ -1,9 +1,14 @@
 from typing import Any
 
 class ForceClass:
-    def __init__(self, name, func) -> None:
+    def __init__(self, name, func, shape=None, dimension_coef=None) -> None:
         self.name = name
         self.func = func
+        self.shape = shape
+        self.dimension_coef = dimension_coef
+        if(self.dimension_coef is not None):
+            if(self.dimension_coef.shape!=self.shape):
+                raise ValueError(f"dimension_coef shape is {self.dimension_coef.shape}. It must be {self.shape}")
         self.F = None
         pass
     def __call__(self, *args: Any, **kwds: Any) -> Any:
@@ -12,4 +17,6 @@ class ForceClass:
         Upon calling the instantiated ForceClass_Base obj, the force is calculated and stored in fd_model.F
         """
         self.F = self.func(*args, **kwds)
+        if(self.dimension_coef is not None):
+            self.F = self.F * self.dimension_coef
         return self.F
