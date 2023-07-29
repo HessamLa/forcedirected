1ed6ff06953a34b6d442560e422ea727d2da92ad
H_ALZ <hessam.la@gmail.com>
2023-07-29 19:35:05 -0400

    batchify function is moved here.

--- forcedirected/utilities/misc.py
+++ forcedirected/utilities/misc.py
@@ -4,5 +4,8 @@ def batchify(idxlist:list, batch_size:int, shuffle=False):
     if(shuffle):
         idxlist = idxlist.copy()
         random.shuffle(idxlist)
+    if(len(idxlist) <= batch_size):
+        yield idxlist
+        return
     for i in range(0, len(idxlist), batch_size):
         yield idxlist[i:i+batch_size]
