a5358197f98269b45187f12e5b4112dfcab2d5bc
H_ALZ <hessam.la@gmail.com>
2023-08-16 19:41:36 -0400

    new module load command

--- fd-batch.sh
+++ fd-batch.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# FD_VERSION='4'
-# FD_VERSION='4nodrop'
+FD_VERSION='4'
+FD_VERSION='4nodrop'
 FD_VERSION='5'
-# FD_VERSION='5z2'
+FD_VERSION='5z2'
+FD_VERSION='6'
+FD_VERSION='7'
 PROGRAM_RUN_CMD="python3 main.py --epochs 5000 --fdversion $FD_VERSION"
 
 if [ $# -eq 0 ]
   then # set the defaults
     PARTITION='gpu-debug'
-elif [ $1 = 'gpu' ] || [ $1 = 'gpu-debug' ]
+elif [ $1 = 'gpu' ] || [ $1 = 'gpu-debug' ] || [ $1 = 'general' ]
   then
     PARTITION=$1
 else
@@ -17,10 +19,10 @@ else
   exit 1
 fi
 
-if [ $PARTITION = 'gpu' ]
+if [ $PARTITION = 'gpu' ] || [ $PARTITION = 'general' ]
   then
     TIME="1-23:59:59"
-elif [ $PARTITION = 'gpu-debug' ]
+elif [ $PARTITION = 'gpu-debug' ] || [ $PARTITION = 'debug' ]
   then
     TIME="3:59:59"
 fi
@@ -29,13 +31,15 @@ fi
 # else if 'BigReg' is in $HOME, then set MODULE_CMD='module load deeplearning/2.12.0;'
 if [[ $HOME == *"Carbonate"* ]]; then
     LOGIN_NODE='carbonate'
-    MODULE_CMD='module load deeplearning/2.12.0; module unload cudatoolkit/11.7'
+    MODULE_CMD='module load python/gpu/3.10.10; module unload cudatoolkit/11.7 '
 elif [[ $HOME == *"BigRed200"* ]]; then
     LOGIN_NODE='bigred200'
-    MODULE_CMD='module load deeplearning/2.12.0'
+    MODULE_CMD='module load python/gpu/3.10.10 '
 else
-    MODULE_CMD='x'
+    MODULE_CMD='echo No module command provided '
 fi
+
+# make the srun systems flag
 
 
 
@@ -74,9 +78,12 @@ function run_sbatch_script() {
   program_cmd="$program_cmd --outputdir_root ./embeddings-$PARTITION-$LOGIN_NODE"
   echo $program_cmd
 
-  sbatch_flags="-p $PARTITION -A general --time=$TIME -J $ndim-$dataset_name --nodes=1 --ntasks-per-node=1 --gpus-per-node=1 --mem=200G"
+  if [ $PARTITION = 'gpu' ] || [ $PARTITION = 'gpu-debug' ]; then
+    sbatch_flags="-p $PARTITION -A general --time=$TIME -J $ndim-$dataset_name --nodes=1 --ntasks-per-node=1 --gpus-per-node=1 --mem=200G"
+  elif [ $PARTITION = 'general' ] || [ $PARTITION = 'debug' ]; then
+    sbatch_flags="-p $PARTITION -A general --time=$TIME -J $ndim-$dataset_name --nodes=1 --ntasks-per-node=1 --mem=200G"
+  fi
   sbatch_flags="$sbatch_flags -o $LOGDIR/fd-%j-$ndim-$dataset_name.txt -e $LOGDIR/fd-%j-$ndim-$dataset_name.err"
-
   sbatch $sbatch_flags --wrap "$MODULE_CMD; $program_cmd"
 }
 
@@ -91,6 +98,9 @@ function run_srun_script() {
   srun $slurm_flags $program_cmd
 }
 
+echo $(date '+%y-%m-%d,%H:%M:%S') >> slurm-jobids.txt
+echo Partition:  $PARTITION >> slurm-jobids.txt
+echo Login Node: $LOGIN_NODE >> slurm-jobids.txt
 
 # run_sbatch_script corafull 24 forcedirected
 
@@ -99,11 +109,28 @@ for ndim in 24 32 64 128 12; do
 # for ndim in 64 128; do
   for method in forcedirected; do
     for dataset_name in cora citeseer ego-facebook wiki pubmed corafull ; do
-      run_sbatch_script "$dataset_name" "$ndim" "$method" 
+      run_sbatch_script "$dataset_name" "$ndim" "$method" >> slurm-jobids.txt 2>&1
     done
   done
 done
 
+
+# python3 main.py --epochs 5000 --fdversion 6 -d pubmed --ndim 128 --outputdir_root ./embeddings-gpu-debug-bigred200
+# Submitted batch job 1870617
+# python3 main.py --epochs 5000 --fdversion 6 -d corafull --ndim 128 --outputdir_root ./embeddings-gpu-debug-bigred200
+# Submitted batch job 1870618
+
+# # for method in forcedirected node2vec deepwalk line sdne struc2vec; do
+# for ndim in 128; do
+# # for ndim in 64 128; do
+#   for method in forcedirected; do
+#     for dataset_name in pubmed corafull ; do
+#       run_sbatch_script "$dataset_name" "$ndim" "$method" >> slurm-jobids.txt 2>&1
+#     done
+#   done
+# done
+
+echo "" >> slurm-jobids.txt
 # for method in forcedirected; do
 #   for ndim in 64 128; do
 #     for dataset_name in cora ego-facebook ; do
