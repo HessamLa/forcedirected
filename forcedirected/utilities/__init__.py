645d4747df2f71695f99c89f57d761f54d332ae0
H_ALZ <hessam.la@gmail.com>
2023-08-21 18:09:47 -0400

    RecursiveName space is added here. Later on I will make this into a python package

--- forcedirected/utilities/__init__.py
+++ forcedirected/utilities/__init__.py
@@ -3,4 +3,5 @@ from .statistics import make_embedding_stats
 from .misc import batchify
 from .optimize_batch_size import optimize_batch_size
 from .optimize_batch_count import optimize_batch_count
+from .RecursiveNamespace import RecursiveNamespace
 
