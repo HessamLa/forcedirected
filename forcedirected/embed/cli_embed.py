0d47eeb4a28f0e78441ee11b5a1fbb90b7a92dfb
HessamLa <hessam.la@gmail.com>
2024-05-07 21:20:15 -0400

    refactored kwargs to options.

--- forcedirected/embed/cli_embed.py
+++ forcedirected/embed/cli_embed.py
@@ -29,16 +29,18 @@ def common_options(func):
     
     @click.option('--seed', type=int, default=None, help='Random seed for reproducibility.', show_default=True)
     @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        kwargs = rns(kwargs)
-        if(kwargs.edgelist is None and kwargs.adjlist is None):
+    def wrapper(*args, **options):
+        options = rns(options)
+        if(options.edgelist is None and options.adjlist is None):
             raise ValueError("Either edgelist or adjlist must be provided.")
-        if(kwargs.filename is None):
-            kwargs.filename = f"{kwargs.name}-d{kwargs.n_dim}.{kwargs.format}"
-        return func(*args, **kwargs)
+        if(options.filename is None):
+            options.filename = f"{options.name}-d{options.n_dim}.{options.format}"
+        return func(*args, **options)
     return wrapper
 
 def save_embeddings(embeddings_df, filepath, format):
+    # get dir
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
     if(format=='csv'):
         embeddings_df.to_csv(filepath, index=False)
     elif(format=='pkl'):
@@ -52,34 +54,34 @@ def save_embeddings(embeddings_df, filepath, format):
 @click.option('--k3', type=float, default=10.0,  help='k3 parameter.')
 @click.option('--k4', type=float, default=0.01,  help='k4 parameter.')
 @click.option('--coeffs', nargs=4, type=float, help='Coefficients for the force calculation. If provided, overrides k1, k2, k3, k4. Used to shorten the syntanx.')
-def fd_basic(**kwargs):
+def fd_basic(**options):
     """Generate embeddings using the force-directed basic algorithm."""
-    kwargs = rns(kwargs)
-    if(kwargs.coeffs is not None):
-        kwargs.k1 = float(kwargs.coeffs[0])
-        kwargs.k2 = float(kwargs.coeffs[1])
-        kwargs.k3 = float(kwargs.coeffs[2])
-        kwargs.k4 = float(kwargs.coeffs[3])
+    options = rns(options)
+    if(options.coeffs is not None):
+        options.k1 = float(options.coeffs[0])
+        options.k2 = float(options.coeffs[1])
+        options.k3 = float(options.coeffs[2])
+        options.k4 = float(options.coeffs[3])
     
     print("fd-basic command with params:")
-    for k,v in kwargs.items():
+    for k,v in options.items():
         print(f"{str(k):<16s}: {v}")
     
     # Make the filename
-    filepath = os.path.join(kwargs.outdir, kwargs.filename)
+    filepath = os.path.join(options.outdir, options.filename)
 
-    if(len(kwargs.edgelist) != 0):
-        print("Input graph path     :", kwargs.edgelist)
-    elif(len(kwargs.adjlist) != 0):
-        print("Input graph path     :", kwargs.adjlist)    
-    print("Embedding dimensions :", kwargs.n_dim)
-    print("Output directory     :", kwargs.outdir)
-    print("Output filename      :", kwargs.filename)
+    if(len(options.edgelist) != 0):
+        print("Input graph path     :", options.edgelist)
+    elif(len(options.adjlist) != 0):
+        print("Input graph path     :", options.adjlist)    
+    print("Embedding dimensions :", options.n_dim)
+    print("Output directory     :", options.outdir)
+    print("Output filename      :", options.filename)
 
     from .fd import embed_basic
-    embeddings_df = embed_basic(**kwargs)
+    embeddings_df = embed_basic(**options)
 
-    save_embeddings(embeddings_df, filepath, kwargs.format)
+    save_embeddings(embeddings_df, filepath, options.format)
     # End of fd_basic 
     #########
 
@@ -89,34 +91,34 @@ def fd_basic(**kwargs):
 @click.option('--k3', type=float, default=10.0,  help='k3 parameter.')
 @click.option('--k4', type=float, default=0.01,  help='k4 parameter.')
 @click.option('--coeffs', nargs=4, type=float, help='Coefficients for the force calculation. If provided, overrides k1, k2, k3, k4. Used to shorten the syntanx.')
-def fd_shell(**kwargs):
+def fd_shell(**options):
     """Generate embeddings using the force-directed basic algorithm with shell averaging."""
-    kwargs = rns(kwargs)
-    if(kwargs.coeffs is not None):
-        kwargs.k1 = float(kwargs.coeffs[0])
-        kwargs.k2 = float(kwargs.coeffs[1])
-        kwargs.k3 = float(kwargs.coeffs[2])
-        kwargs.k4 = float(kwargs.coeffs[3])
+    options = rns(options)
+    if(options.coeffs is not None):
+        options.k1 = float(options.coeffs[0])
+        options.k2 = float(options.coeffs[1])
+        options.k3 = float(options.coeffs[2])
+        options.k4 = float(options.coeffs[3])
     
     print("fd-basic command with params:")
-    for k,v in kwargs.items():
+    for k,v in options.items():
         print(f"{str(k):<16s}: {v}")
     
     # Make the filename
-    filepath = os.path.join(kwargs.outdir, kwargs.filename)
+    filepath = os.path.join(options.outdir, options.filename)
 
-    if(len(kwargs.edgelist) != 0):
-        print("Input graph path     :", kwargs.edgelist)
-    elif(len(kwargs.adjlist) != 0):
-        print("Input graph path     :", kwargs.adjlist)    
-    print("Embedding dimensions :", kwargs.n_dim)
-    print("Output directory     :", kwargs.outdir)
-    print("Output filename      :", kwargs.filename)
+    if(len(options.edgelist) != 0):
+        print("Input graph path     :", options.edgelist)
+    elif(len(options.adjlist) != 0):
+        print("Input graph path     :", options.adjlist)    
+    print("Embedding dimensions :", options.n_dim)
+    print("Output directory     :", options.outdir)
+    print("Output filename      :", options.filename)
 
     from .fd import embed_shell
-    embeddings_df = embed_shell(**kwargs)
+    embeddings_df = embed_shell(**options)
 
-    save_embeddings(embeddings_df, filepath, kwargs.format)
+    save_embeddings(embeddings_df, filepath, options.format)
     # End of fd_shell 
     #########
 
@@ -130,35 +132,35 @@ def fd_shell(**kwargs):
 @click.option('-L', '--landmarks_ratio', type=click.FloatRange(0, 1.0), default=0.01, help='Ratio of landmark nodes (top ratio of degrees).')
 @click.option('--coeffs', nargs=7, type=(float, float, float, float, int, int, click.FloatRange(0, 1.0)), 
               help='Coefficients for the force calculation. If provided, overrides other parameters. Used to shorten the syntanx.')
-def fd_targets(**kwargs):
+def fd_targets(**options):
     """Generate embeddings using the force-directed algorithm with selective target nodes."""
-    kwargs = rns(kwargs)
-    if(kwargs.coeffs is not None):
-        kwargs.k1 = kwargs.coeffs[0]
-        kwargs.k2 = kwargs.coeffs[1]
-        kwargs.k3 = kwargs.coeffs[2]
-        kwargs.k4 = kwargs.coeffs[3]
-        kwargs.reach_a = kwargs.coeffs[4]
-        kwargs.reach_r = kwargs.coeffs[5]
-        kwargs.landmarks_ratio = kwargs.coeffs[6]
+    options = rns(options)
+    if(options.coeffs is not None):
+        options.k1 = options.coeffs[0]
+        options.k2 = options.coeffs[1]
+        options.k3 = options.coeffs[2]
+        options.k4 = options.coeffs[3]
+        options.reach_a = options.coeffs[4]
+        options.reach_r = options.coeffs[5]
+        options.landmarks_ratio = options.coeffs[6]
 
     print("fd-landmark command with params:")
-    for k,v in kwargs.items():
+    for k,v in options.items():
         print(f"{str(k):<16s}: {v}")
     
-    filepath = os.path.join(kwargs.outdir, kwargs.filename)
+    filepath = os.path.join(options.outdir, options.filename)
 
-    if(len(kwargs.edgelist) != 0):
-        print("Input graph path     :", kwargs.edgelist)
-    elif(len(kwargs.adjlist) != 0):
-        print("Input graph path     :", kwargs.adjlist)    
-    print("Embedding dimensions :", kwargs.n_dim)
-    print("Output directory     :", kwargs.outdir)
-    print("Output filename      :", kwargs.filename)
+    if(len(options.edgelist) != 0):
+        print("Input graph path     :", options.edgelist)
+    elif(len(options.adjlist) != 0):
+        print("Input graph path     :", options.adjlist)    
+    print("Embedding dimensions :", options.n_dim)
+    print("Output directory     :", options.outdir)
+    print("Output filename      :", options.filename)
 
     from .fd import embed_targets
-    embeddings_df = embed_targets(**kwargs)
+    embeddings_df = embed_targets(**options)
 
-    save_embeddings(embeddings_df, filepath, kwargs.format)
+    save_embeddings(embeddings_df, filepath, options.format)
     # End of fd_targets
     #########
