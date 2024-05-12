a76a20b23a6fb9eeb87d50194c315d48e42a0db5
HessamLa <hessam.la@gmail.com>
2024-05-12 13:24:43 -0400

    updated the command functions

--- forcedirected/embed/cli_embed.py
+++ forcedirected/embed/cli_embed.py
@@ -1,12 +1,12 @@
 import click
+@click.group()
+def cli_embed():
+    """Main entry point for the graph embedding CLI tool."""
+    pass
+
 import os
 import functools
 from recursivenamespace import rns
-
-@click.group()
-def cli_embed():
-    """Main entry point for the graph generation CLI tool."""
-    pass
 
 def common_options(func):
     """Decorator to apply common options to graph generation commands."""
@@ -51,28 +51,15 @@ def save_embeddings(embeddings_df, filepath, format='csv', set_column_names=Fals
         embeddings_df.to_pickle(filepath)
     print(f"Embeddings saved to {filepath}")
 
-@common_options
-# @click.option('--model', type=str, default='model_201_basic', help='Path to the embeddings file.', show_default=True)
-@click.option('--k1', type=float, default=0.999, help='k1 parameter.')
-@click.option('--k2', type=float, default=1.0,   help='k2 parameter.')
-@click.option('--k3', type=float, default=10.0,  help='k3 parameter.')
-@click.option('--k4', type=float, default=0.01,  help='k4 parameter.')
-@click.option('--coeffs', nargs=4, type=float, help='Coefficients for the force calculation. If provided, overrides k1, k2, k3, k4. Used to shorten the syntanx.')
-def fdbasic(**options):
-    """Generate embeddings using the force-directed basic algorithm."""
+
+def fd_base(**options):
     options = rns(options)
-    if(options.coeffs is not None):
-        options.k1 = float(options.coeffs[0])
-        options.k2 = float(options.coeffs[1])
-        options.k3 = float(options.coeffs[2])
-        options.k4 = float(options.coeffs[3])
-    
+
     # Make the filename
     if(options.filename is None):
-        options.filename = f"{options.name}-fdbasic-d{options.n_dim}.{options.format}"
-    filepath = os.path.join(options.outdir, options.filename)
-
-    print("fd-landmark command with params:")
+        raise ValueError("filename must be provided.")
+    
+    print("command with params:")
     for k,v in options.items():
         print(f"{str(k):<16s}: {v}")
     
@@ -84,10 +71,54 @@ def fdbasic(**options):
     print("Output directory     :", options.outdir)
     print("Output filename      :", options.filename)
 
-    from .fd import embed_basic
-    embeddings_df = embed_basic(**options)
+    from .fd import embed
+    embeddings_df = embed(**options)
 
+    filepath = os.path.join(options.outdir, options.filename)
     save_embeddings(embeddings_df, filepath, options.format, set_column_names=True)
+
+@common_options
+@click.option('--model', 'model_module', type=str, help='Path to the embeddings module. Ex. model_201_basic.', show_default=True, required=True)
+@click.option('--k1', type=float, default=0.999, help='k1 parameter.')
+@click.option('--k2', type=float, default=1.0,   help='k2 parameter.')
+@click.option('--k3', type=float, default=10.0,  help='k3 parameter.')
+@click.option('--k4', type=float, default=0.01,  help='k4 parameter.')
+@click.option('--coeffs', nargs=4, type=float, help='Coefficients for the force calculation. If provided, overrides k1, k2, k3, k4. Used to shorten the syntanx.')
+def fd(**options):
+    """Generate embeddings using the force-directed algorithm, with the provided model."""
+    options = rns(options)
+    if(options.filename is None):
+        options.filename = f"{options.name}-{options.model_module}-d{options.n_dim}.{options.format}"
+
+    fd_base(**options)
+    return
+    # End of fd_basic 
+    #########
+
+@common_options
+# @click.option('--model', type=str, default='model_201_basic', help='Path to the embeddings file.', show_default=True)
+@click.option('--k1', type=float, default=0.999, help='k1 parameter.')
+@click.option('--k2', type=float, default=1.0,   help='k2 parameter.')
+@click.option('--k3', type=float, default=10.0,  help='k3 parameter.')
+@click.option('--k4', type=float, default=0.01,  help='k4 parameter.')
+@click.option('--coeffs', nargs=4, type=float, help='Coefficients for the force calculation. If provided, overrides k1, k2, k3, k4. Used to shorten the syntanx.')
+def fdbasic(**options):
+    """Generate embeddings using the force-directed basic algorithm."""
+    options = rns(options)
+    # set the parameters
+    if(options.coeffs is not None):
+        options.k1 = float(options.coeffs[0])
+        options.k2 = float(options.coeffs[1])
+        options.k3 = float(options.coeffs[2])
+        options.k4 = float(options.coeffs[3])
+    # Make the filename
+    if(options.filename is None):
+        options.filename = f"{options.name}-fdbasic-d{options.n_dim}.{options.format}"
+
+    from forcedirected.models import model_201_basic
+    options.model_module = model_201_basic
+    fd_base(**options)
+    return
     # End of fd_basic 
     #########
 
@@ -105,29 +136,14 @@ def fdshell(**options):
         options.k2 = float(options.coeffs[1])
         options.k3 = float(options.coeffs[2])
         options.k4 = float(options.coeffs[3])
-    
-    # Make the filename
     if(options.filename is None):
         options.filename = f"{options.name}-fdshell-d{options.n_dim}.{options.format}"
-    filepath = os.path.join(options.outdir, options.filename)
 
-    print("fd-landmark command with params:")
-    for k,v in options.items():
-        print(f"{str(k):<16s}: {v}")
-    
-    if(len(options.edgelist) != 0):
-        print("Input graph path     :", options.edgelist)
-    elif(len(options.adjlist) != 0):
-        print("Input graph path     :", options.adjlist)    
-    print("Embedding dimensions :", options.n_dim)
-    print("Output directory     :", options.outdir)
-    print("Output filename      :", options.filename)
-
-    from .fd import embed_shell
-    embeddings_df = embed_shell(**options)
-
-    save_embeddings(embeddings_df, filepath, options.format, set_column_names=True)
-    # End of fd_shell 
+    from forcedirected.models import model_204_shell
+    options.model_module = model_204_shell
+    fd_base(**options)
+    return
+    # End of fdshell 
     #########
 
 @common_options
@@ -143,6 +159,7 @@ def fdshell(**options):
 def fdtargets(**options):
     """Generate embeddings using the force-directed algorithm with selective target nodes."""
     options = rns(options)
+    # set the parameters
     if(options.coeffs is not None):
         options.k1 = options.coeffs[0]
         options.k2 = options.coeffs[1]
@@ -151,28 +168,14 @@ def fdtargets(**options):
         options.reach_a = options.coeffs[4]
         options.reach_r = options.coeffs[5]
         options.landmarks_ratio = options.coeffs[6]
-
     if(options.filename is None):
         options.filename = f"{options.name}-fdtargets-d{options.n_dim}.{options.format}"
-    filepath = os.path.join(options.outdir, options.filename)
 
-    print("fd-landmark command with params:")
-    for k,v in options.items():
-        print(f"{str(k):<16s}: {v}")
-    
-    if(len(options.edgelist) != 0):
-        print("Input graph path     :", options.edgelist)
-    elif(len(options.adjlist) != 0):
-        print("Input graph path     :", options.adjlist)    
-    print("Embedding dimensions :", options.n_dim)
-    print("Output directory     :", options.outdir)
-    print("Output filename      :", options.filename)
-
-    from .fd import embed_targets
-    embeddings_df = embed_targets(**options)
-
-    save_embeddings(embeddings_df, filepath, options.format, set_column_names=True)
-    # End of fd_targets
+    from forcedirected.models import model_214_targets
+    options.model_module = model_214_targets
+    fd_base(**options)
+    return
+    # End of fdtargets
     #########
 
 # node2vec
