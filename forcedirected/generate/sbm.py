updated the lfr and sbm modules. Changed lfr.generate() ot lfr(). Same for sbm. Fixed the exclusive condition between min-degree and average-degree with lfr. Now it works better.

forcedirected/generate/sbm.py
@@ -5,10 +5,10 @@ import numpy as np
 
 
 __all__ = [
-    "generate_sbm",
+    "sbm",
 ]
 
-def generate_sbm(n_communities, p_intra, p_inter, seed=None, **kwargs):
+def sbm(n_communities, p_intra, p_inter, seed=None, **kwargs):
     # print(f"Generating graph using Stochastic Block Model with parameters: n={n_nodes}, gamma={gamma}, beta={beta}, mu={mu}, min_degree={min_degree}, max_degree={max_degree}, average_degree={average_degree}, min_community={min_community}, max_community={max_community}.")
     n_nodes = kwargs.get("n_nodes", 500)
     print(f"Generating graph using Stochastic Block Model with parameters: n={n_nodes}, ....")
@@ -17,6 +17,8 @@ def generate_sbm(n_communities, p_intra, p_inter, seed=None, **kwargs):
     prob_matrix = np.full((n_communities, n_communities), p_inter)
     np.fill_diagonal(prob_matrix, p_intra)
     G = nx.stochastic_block_model(sizes, prob_matrix)
+    # for node, comm in nx.get_node_attributes(G, 'block').items():
+    #     G.nodes[node]['community'] = G.nodes[node].pop('block')
     return G
     # # change 'block' to 'community'
     # for node, comm in nx.get_node_attributes(G, 'block').items():
@@ -34,6 +36,6 @@ if __name__ == '__main__':
     print("Example usage")
     n = 1000
     outpath = "./sbm.edgelist"
-    G = generate_sbm(n_nodes=500, n_communities=5, p_intra=0.25, p_inter=0.01,seed=None)
+    G = sbm(n_nodes=500, n_communities=5, p_intra=0.25, p_inter=0.01,seed=None)
     
     # print(f"SBM graph generated at {outpath} with node count {n} and parameters .....")